0c183401ab8eb9533e18a182ccff6c1c382a427a
yene <yene@cocoadojo.com>
2014-03-14 15:03:16 +0100

    added code to cricle through the gifs

--- JustSomeGifs/XXXViewController.m
+++ JustSomeGifs/XXXViewController.m
@@ -11,6 +11,8 @@
 
 @interface XXXViewController () {
   BOOL isFaved;
+  NSArray *gifs;
+  NSUInteger position;
 }
 @end
 
@@ -20,11 +22,42 @@
   return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
 }
 
+- (void)setup;
+{
+  NSArray *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:[self documentsDirectory] error:nil];
+  
+  if ([files count] != 0) return;
+  
+  // folder is empty, do first time setup
+  [self copyGifWithName:@"cRozqMJ"];
+  [self copyGifWithName:@"tqdCDsG"];
+  [self copyGifWithName:@"1eU2UEn"];
+  [self copyGifWithName:@"F8oNz3Q"];
+  [self copyGifWithName:@"uSaXek5"];
+}
+
+- (void)copyGifWithName:(NSString *)name;
+{
+  NSString *resourcePath = [[NSBundle mainBundle] pathForResource:name ofType:@"gif"];
+  NSString *gifFileName = [NSString stringWithFormat:@"%@.gif", name];
+  NSString *gifPath = [[self documentsDirectory] stringByAppendingPathComponent:gifFileName];
+  [[NSFileManager defaultManager] copyItemAtPath:resourcePath toPath:gifPath error:nil];
+}
+
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+  [self setup];
+  position = 0;
+  
+  [self showGif];
+}
 
-  NSURL *url = [[NSBundle mainBundle] URLForResource:@"cRozqMJ" withExtension:@"gif"];
+- (void)showGif;
+{
+  gifs = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:[self documentsDirectory] error:nil];
+  NSString *path = [[self documentsDirectory] stringByAppendingPathComponent:gifs[position]];
+  NSURL *url = [NSURL fileURLWithPath:path];
   self.gifView.image = [UIImage animatedImageWithAnimatedGIFData:[NSData dataWithContentsOfURL:url]];
 }
 
@@ -37,12 +70,19 @@
 - (IBAction)toggleFav:(id)sender {
   if (isFaved) {
     [self.starButton setImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
+  } else {
+    [self.starButton setImage:[UIImage imageNamed:@"star2.png"] forState:UIControlStateNormal];
   }
+  isFaved = !isFaved;
 }
 
 - (IBAction)previousGif:(id)sender {
+  position = (position == 0) ? [gifs count]-1 : --position;
+  [self showGif];
 }
 
 - (IBAction)nextGif:(id)sender {
+  position = ++position % [gifs count];
+  [self showGif];
 }
 @end
